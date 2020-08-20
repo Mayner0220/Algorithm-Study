c84e18728394349ef2ea5069f8276f420e7e7192
Mayner <shj030220@gmail.com>
2020-08-20 11:58:32 +0900

    Programmers

--- Stack&Queue/Stock_price.py
+++ Stack&Queue/Stock_price.py
@@ -1,4 +1,11 @@
 def solution(input):
-    time = []
-    for Index in range(0, len(input)):
-        for 
+    time = [0] * len(input)
+
+    for Index in range(len(input) - 1):
+        for Check in range(Index, len(input)-1):
+            if input[Index] > input[Check]:
+                break
+            else:
+                time[Index] += 1
+
+    return time
