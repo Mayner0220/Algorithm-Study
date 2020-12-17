4ee1dd71879fbeb433f24fa707200f200bc090ec
Shim Jung Hyun <shj030220@gmail.com>
2020-12-17 20:09:46 +0900

    BaekJoon

--- Graph/Save_the_princess.py
+++ Graph/Save_the_princess.py
@@ -6,12 +6,14 @@ q = deque()
 n, m, t = map(int, input().split())
 map = [list(map(int, list(input().split()))) for _ in range(n)]
 visited = [[-1] * m for _ in range(n)]
+get_sword = 999999999999999
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 def BFS():
-    global t
-    get_sword = 999999999
+    global get_sword
+    q.append((0, 0))
+    visited[0][0] = 1
 
     while q:
         x, y = q.popleft()
@@ -20,19 +22,17 @@ def BFS():
             get_sword = n-x-1 + m-y-1 + visited[x][y]-1
 
         if x==n-1 and y==m-1:
-            return min(get_sword, visited[x][y]-1)
+            return min(visited[x][y]-1, get_sword)
 
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx, ny = x+dx[i], y+dy[i]
 
-            if 0 <=nx<n and 0<=ny<m and map[nx][ny]!=1:
-                if(visited[nx][ny]==-1):
-                    q.append((nx, ny))
+            if 0<=nx<n and 0<=ny<m and map[nx][ny]!=1:
+                if visited[nx][ny]==-1:
                     visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx, ny))
 
     return get_sword
 
-q.append((0, 0))
-visited[0][0] = 1
-res = BFS()
-print(res if res<t else "Fail")
+result = BFS()
+print(result if result<t else "Fail")
